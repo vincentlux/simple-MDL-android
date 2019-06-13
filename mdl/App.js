a14ed72c938bd5b6281485f14528a1d977eee9a1
vincentlunc <xiaopeng@live.unc.edu>
2019-06-13 14:12:49 -0400

    finish split

--- mdl/App.js
+++ mdl/App.js
@@ -13,86 +13,12 @@ import UploadArchive from './components/UploadArchive';
 import VoiceButton from './components/VoiceButton';
 
 
-import FilePickerManager from 'react-native-file-picker';
-import RNFetchBlob from 'rn-fetch-blob';
-import Voice from 'react-native-voice';
-
-
-// type Props = {};
-// export default class App extends Component<Props> {
 export default class App extends React.Component {
-
-  state = {
-      file: undefined,
-      txt: undefined,
-  };
-
-  // getUserLocationHandler = () => {
-  //   navigator.geolocation.getCurrentPosition(position => {
-  //     console.log(position);
-  //   }, err => console.log(err));
-  // }
-
-  uploadText = () =>{
-    console.log('uploading')
-    RNFetchBlob.config({
-      trusty : true
-    })
-    .fetch('POST', 'https://mdl.unc.edu/api/upload_file_rn', {
-      'Content-Type' : 'multipart/form-data',
-    }, [
-      { name : 'name', data : 'user'},
-      // element with property `filename` will be transformed into `file` in form data
-      { name : 'file', filename: this.state.fileName, type:'text/*', data: RNFetchBlob.wrap(this.state.txt)},
-    ]).then((resp) => {
-      console.log('uploaded')
-      console.log(resp)
-    }).catch((err) => {
-      console.log(err)
-    })
-  }
-
-  selectFileTapped = () => {
-    const options = {
-      title: 'File Picker',
-      chooseFileButtonTitle: 'Choose File...'
-    };
-
-    FilePickerManager.showFilePicker(options, (response) =>{
-      console.log('Response=', response);
-      if (response.didCancel) {
-        console.log('User cancelled file picker');
-      }
-      else if (response.error) {
-        console.log('FilePickerManager Error: ', response.error);
-      }
-      else if (response.customButton) {
-        console.log('User tapped custom button: ', response.customButton);
-      }
-      else {
-        this.setState({
-          file: response,
-          txt: response.uri,
-          fileName: response.path.replace(/\//g, '_') // convert all / to _
-        });
-        console.log('hey',this.state.file)
-        console.log(this.state.file.path.replace(/\//g, '_'))
-        console.log('txt?',this.state.txt)
-        
-      }
-    });
-  }
-  
 
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity style={styles.button} onPress={this.selectFileTapped.bind(this)}>
-              <Text style={styles.text}>Choose file..</Text>
-        </TouchableOpacity>
-        {/*<Text style={styles.fileInfo}>{JSON.stringify(this.state.file)}</Text>*/}
-        {/*<FetchLocation onGetLocation={this.getUserLocationHandler} />*/}
-        <UploadArchive onUploadArchive={this.uploadText}/>
+        <UploadArchive/>
         <VoiceButton/>
 
       </View>
@@ -114,6 +40,10 @@ const styles = StyleSheet.create({
   },
   text: {
     color: '#fff',
+    fontSize: 20,
+  },
+  test: {
+    color: '#0b45a3',
     fontSize: 20,
   },
   button: {
--- mdl/components/UploadArchive.js
+++ mdl/components/UploadArchive.js
@@ -1,10 +1,87 @@
 import React, { Component } from 'react';
-import { Text, StyleSheet, Button, TouchableOpacity, PixelRatio } from 'react-native';
+import { Text, StyleSheet, Button, View, TouchableOpacity, PixelRatio } from 'react-native';
 
-// class uploadArchive extends Component {
+import FilePickerManager from 'react-native-file-picker';
+import RNFetchBlob from 'rn-fetch-blob';
 
+class UploadArchive extends Component {
+    state = {
+        file: undefined,
+        txt: undefined,
+    };
+  
+    // getUserLocationHandler = () => {
+    //   navigator.geolocation.getCurrentPosition(position => {
+    //     console.log(position);
+    //   }, err => console.log(err));
+    // }
+  
+    uploadText = () =>{
+      console.log('uploading')
+      RNFetchBlob.config({
+        trusty : true
+      })
+      .fetch('POST', 'https://mdl.unc.edu/api/upload_file_rn', {
+        'Content-Type' : 'multipart/form-data',
+      }, [
+        { name : 'name', data : 'user'},
+        // element with property `filename` will be transformed into `file` in form data
+        { name : 'file', filename: this.state.fileName, type:'text/*', data: RNFetchBlob.wrap(this.state.txt)},
+      ]).then((resp) => {
+        console.log('uploaded')
+        console.log(resp)
+      }).catch((err) => {
+        console.log(err)
+      })
+    }
+  
+    selectFileTapped = () => {
+      const options = {
+        title: 'File Picker',
+        chooseFileButtonTitle: 'Choose File...'
+      };
+  
+      FilePickerManager.showFilePicker(options, (response) =>{
+        console.log('Response=', response);
+        if (response.didCancel) {
+          console.log('User cancelled file picker');
+        }
+        else if (response.error) {
+          console.log('FilePickerManager Error: ', response.error);
+        }
+        else if (response.customButton) {
+          console.log('User tapped custom button: ', response.customButton);
+        }
+        else {
+          this.setState({
+            file: response,
+            txt: response.uri,
+            fileName: response.path.replace(/\//g, '_') // convert all / to _
+          });
+          console.log('hey',this.state.file)
+          console.log(this.state.file.path.replace(/\//g, '_'))
+          console.log('txt?',this.state.txt)
+          
+        }
+      });
+    }
+    render() {
+        return (
+          <View style={styles.container}>
+            <TouchableOpacity style={styles.button} onPress={this.selectFileTapped.bind(this)}>
+                  <Text style={styles.text}>Choose file..</Text>
+            </TouchableOpacity>
+            {/*<Text style={styles.fileInfo}>{JSON.stringify(this.state.file)}</Text>*/}
+            {/*<FetchLocation onGetLocation={this.getUserLocationHandler} />*/}
+            <TouchableOpacity style={styles.button} onPress={this.onUploadArchive}>
+                <Text style={styles.text} >Upload!</Text>
+            </TouchableOpacity>
+    
+          </View>
+        );
+      }
 
-// };
+};
 
 
 
@@ -20,19 +97,37 @@ const uploadArchive = props => {
 
 };
 
-export default uploadArchive;
+
 
 const styles = StyleSheet.create({
-    text: {
-      color: '#fff',
-      fontSize: 20,
-    },
-    button: {
-      // borderColor: '#9B9B9B',
-      borderWidth: 1 / PixelRatio.get(),
-      backgroundColor: '#8543e8',
-      margin: 10,
-      padding: 10
-    },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+      },
+      welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+      },
+      text: {
+        color: '#fff',
+        fontSize: 20,
+      },
+      button: {
+        // borderColor: '#9B9B9B',
+        borderWidth: 1 / PixelRatio.get(),
+        backgroundColor: '#8543e8',
+        margin: 10,
+        padding: 10
+      },
+      fileInfo: {
+        borderColor: '#9B9B9B',
+        borderWidth: 1 / PixelRatio.get(),
+        margin: 5,
+        padding: 5
+      },
+});
 
-  });
+export default UploadArchive;
