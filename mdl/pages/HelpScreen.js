6cc51df0befdf1f2718cd862b4f7b4dc6c1f2ad1
vincentlunc <xiaopeng@live.unc.edu>
2019-06-21 23:24:27 -0400

    add temp help

--- mdl/pages/HelpScreen.js
+++ mdl/pages/HelpScreen.js
@@ -15,17 +15,33 @@ class HelpScreen extends React.Component {
     };
     render() {
       return (
-          <WebView
-           source={{ uri: 'https://mdl.unc.edu' }}
-           originWhitelist={['https://*', 'git://*']}
-           javaScriptEnabled={true}
-           domStorageEnabled={true} 
-           startInLoadingState={false}
-           />
+        <Text>?ON "Soccer" => All emails containing "soccer" at either header or subject{"\n\n"}
+        ?ON "Soccer" full => Same as ?ON "Soccer"{"\n\n"}
+        ?ON "Soccer" subject => All emails containing "soccer" only at subject{"\n\n"}
+        ?"mike" => All emails from Mike{"\n\n"}
+        ?'Mike' 'Drake' 'Jim' => All emails from Mike, Drake, or Jim{"\n\n"}
+        ?LAST => Latest one email{"\n\n"}
+        ?LAST 1 => Latest one email{"\n\n"}
+        ?LAST 1 month EMAIL 'Drake' => Email in last month from Drake{"\n\n"}
+        ?LAST EMAIL 'Drake' 'Jim' => Latest email from either Drake or Jim{"\n\n"}
+        ?LAST 1 EMAIL 'Drake' => Latest email from Drake{"\n\n"}
+        ?TOTAL => All emails{"\n\n"}
+        ?'Mike' ON 'Soccer' MSWORD LAST => Latest email from Mike with soccer at header and MSWORD appeared in the email{"\n\n"}
+        </Text>
+        
       );
     }
   }
 
+
+
+  //           <WebView
+  // source={{ uri: 'https://mdl.unc.edu' }}
+  // originWhitelist={['https://*', 'git://*']}
+  // javaScriptEnabled={true}
+  // domStorageEnabled={true} 
+  // startInLoadingState={false}
+  // />
 
 const styles = StyleSheet.create({
 container: {
